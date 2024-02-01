f46fcd8cd38770e78beeabe9903bf69147cc8dab
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-01-31 23:57:47 -0500

    added without binary search

--- searches/binary_search.py
+++ searches/binary_search.py
@@ -1,4 +1,4 @@
-# recursive way to search the question  
+# recursive way to search the array  
 def binary_search(arr, low, high, item):
    if low <= high:
       mid = (low + high) // 2
@@ -11,4 +11,19 @@ def binary_search(arr, low, high, item):
          return binary_search(arr, mid+1, high, item)
    else:
       return -1
-   
+
+# without recursive to search array
+def binary_search(l, key):
+   sorted_array =  l.sort()
+   low, high = 0, len(l) - 1
+   while low <= high:
+      mid = (low+high) // 2
+      if key == l[mid]:
+         return mid
+      elif key > l[mid]:
+         low = mid + 1
+      elif key < l[mid]:
+         high = mid - 1
+   return "not found"
+a = [3,5,6,7,8]
+print(binary_search(a,8))
