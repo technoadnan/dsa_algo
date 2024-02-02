c9864e8952c8ce65619fe0c912aa8e3155d6c834
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-01 23:07:16 -0500

    bubble sort

---
+++ sorting_algo/bubble_sort.py
@@ -0,0 +1,13 @@
+def bubble_sort(arr):
+
+   for i in range(len(arr) - 1):
+      flag = 0
+      for j in range(len(arr) - 1 - i):
+         if arr[j] > arr[j+1]:
+         arr[j],arr[j+1] = arr[j+1],arr[j]
+         flag =1
+
+      if flag == 0:
+         break
+   
+   return arr
