77e682766d7d66afb9d26a64f1d89b5b246ce51a
Adnan Bin Alam <code.technoadnan@gmail.com>
2024-02-02 22:48:54 -0500

    fixed indentation

--- sorting_algo/bubble_sort.py
+++ sorting_algo/bubble_sort.py
@@ -4,10 +4,12 @@ def bubble_sort(arr):
       flag = 0
       for j in range(len(arr) - 1 - i):
          if arr[j] > arr[j+1]:
-         arr[j],arr[j+1] = arr[j+1],arr[j]
-         flag =1
+            arr[j],arr[j+1] = arr[j+1],arr[j]
+            flag = 1
 
       if flag == 0:
          break
    
    return arr
+
+
